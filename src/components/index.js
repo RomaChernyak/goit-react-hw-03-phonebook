3bf952f8f06e6a733f2735908ebc2f552fef1915
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-07-18 23:56:51 +0300

    starting point

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import { App } from "./App";
+import { ContactForm } from "./ContactForm";
+import { ContactList } from "./ContactList";
+import { Filter } from "./Filter";
+
+export { App, ContactForm, ContactList, Filter };
