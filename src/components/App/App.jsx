c976f1070a087bb8bd94f30bb0f658277a16c04b
RomaChernyak <118313101+RomaChernyak@users.noreply.github.com>
2023-07-21 14:53:44 +0300

    transfer default contacts to separate json

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,21 @@
 import React, { Component } from "react";
 import Notiflix from "notiflix";
-import { ContactForm, ContactList, Filter } from "components";
+import { ContactForm, ContactList, Filter, defaultContacts } from "components";
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [...defaultContacts],
 
-    filter: ''
+    filter: '',
   };
 
   componentDidMount() {
-    console.log("App componentDidMount");
-
     const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
       this.setState({ contacts: parsedContacts });
     }
-      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   };
 
   componentDidUpdate(_, prevState) {
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import { App } from "./App";
 import { ContactForm } from "./ContactForm";
 import { ContactList } from "./ContactList";
 import { Filter } from "./Filter";
+import defaultContacts from "../backend/defaultContacts.json";
 
-export { App, ContactForm, ContactList, Filter };
+export { App, ContactForm, ContactList, Filter, defaultContacts };
